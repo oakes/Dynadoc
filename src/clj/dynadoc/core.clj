4bda6d8e870d0c5aaa2b8bda5a482df9e064e222
oakes <zach@sekao.net>
2017-11-19 22:53:40 -0500

    Use some-> to prevent NPE

--- src/clj/dynadoc/core.clj
+++ src/clj/dynadoc/core.clj
@@ -162,7 +162,7 @@
                                  (get-cljs-vars cljs-nses-and-vars ns-sym))]
                       ns-sym (get-cljs-vars cljs-nses-and-vars ns-sym))
                nil)
-        state (atom {:type (keyword (name type))
+        state (atom {:type (some-> type name keyword)
                      :nses nses
                      :ns-sym ns-sym
                      :ns-meta (when (= type 'clj)
