0e61ff551644e23970ad47af2b5e69aa7be7c52f
oakes <zach@sekao.net>
2017-12-03 12:29:30 -0500

    Increment template version

--- lein-dynadoc/src/leiningen/dynadoc.clj
+++ lein-dynadoc/src/leiningen/dynadoc.clj
@@ -20,7 +20,7 @@
   (eval/eval-in-project
     (deps/add-if-missing
       project
-      '[dynadoc/lein-dynadoc "1.1.2"])
+      '[dynadoc/lein-dynadoc "1.1.3"])
     `(do
        (dynadoc.core/start
          {:port ~port :ip ~host})
