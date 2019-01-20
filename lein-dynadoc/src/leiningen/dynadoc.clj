4d1d21f494af241249bf71c76eb158b1be628ca4
oakes <zach@sekao.net>
2019-01-20 07:15:24 -0600

    Update template

--- lein-dynadoc/src/leiningen/dynadoc.clj
+++ lein-dynadoc/src/leiningen/dynadoc.clj
@@ -12,7 +12,7 @@
   (eval/eval-in-project
     (deps/add-if-missing
       project
-      '[dynadoc/lein-dynadoc "1.5.0"])
+      '[dynadoc/lein-dynadoc "1.5.1"])
     `(do
        (dynadoc.core/start ~options)
        (when '~main (require '~main)))
