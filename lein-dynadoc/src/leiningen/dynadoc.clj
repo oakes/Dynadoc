af3a5c7a4cf23eae8e86955b6852e4240bb7beff
oakes <zach@sekao.net>
2019-02-16 16:23:42 -0600

    Increment version number

--- lein-dynadoc/src/leiningen/dynadoc.clj
+++ lein-dynadoc/src/leiningen/dynadoc.clj
@@ -12,7 +12,7 @@
   (eval/eval-in-project
     (deps/add-if-missing
       project
-      '[dynadoc/lein-dynadoc "1.5.5"])
+      '[dynadoc/lein-dynadoc "1.5.6"])
     `(do
        (dynadoc.core/start ~options)
        (when '~main (require '~main)))
