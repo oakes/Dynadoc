e74ad7532193dfe6fd1de7abaa8d0fc6c14f19b5
oakes <zach@sekao.net>
2019-02-16 16:51:56 -0600

    Enable expound

--- dev.clj
+++ dev.clj
@@ -1,8 +1,11 @@
 (require
   '[orchestra.spec.test :as st]
+  '[expound.alpha :as expound]
+  '[clojure.spec.alpha :as s]
   '[figwheel.main :as figwheel]
   '[dynadoc.core :as dynadoc])
 
 (st/instrument)
+(alter-var-root #'s/*explain-out* (constantly expound/printer))
 (dynadoc/dev-start {:port 5000 :dev? true})
 (figwheel/-main "--build" "dev")
--- src/dynadoc/dev.cljs
+++ src/dynadoc/dev.cljs
@@ -1,6 +1,9 @@
 (ns dynadoc.dev
   (:require [dynadoc.core]
             [dynadoc.examples]
-            [orchestra-cljs.spec.test :as st]))
+            [orchestra-cljs.spec.test :as st]
+            [expound.alpha :as expound]
+            [clojure.spec.alpha :as s]))
 
 (st/instrument)
+(set! s/*explain-out* expound/printer)
