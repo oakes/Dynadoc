fae9da27ff9129f6d89feacadc4e3de47e476a7f
oakes <zach@sekao.net>
2019-02-16 15:36:45 -0600

    Instrument during dev

--- dev.clj
+++ dev.clj
@@ -1,7 +1,8 @@
 (require
-  '[orchestra.spec.test :refer [instrument]]
+  '[orchestra.spec.test :as st]
   '[figwheel.main :as figwheel]
   '[dynadoc.core :as dynadoc])
 
+(st/instrument)
 (dynadoc/dev-start {:port 5000 :dev? true})
 (figwheel/-main "--build" "dev")
