fecee8b71f808a0b6e2f2f1492cd26e79013100e
oakes <zach@sekao.net>
2019-03-13 07:08:37 -0600

    Increment version number

--- lein-dynadoc/project.clj
+++ lein-dynadoc/project.clj
@@ -1,9 +1,9 @@
-(defproject dynadoc/lein-dynadoc "1.6.0"
+(defproject dynadoc/lein-dynadoc "1.6.1"
   :description "A conveninent Dynadoc launcher for Leiningen projects"
   :url "https://github.com/oakes/Dynadoc"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[dynadoc "1.6.0"]
+  :dependencies [[dynadoc "1.6.1"]
                  [leinjacker "0.4.2"]
                  [org.clojure/tools.cli "0.3.5"]]
   :repositories [["clojars" {:url "https://clojars.org/repo"
