b2c1a9f22b48cbf327a86d668c49cec0a59246fe
oakes <zach@sekao.net>
2019-03-19 23:10:26 -0600

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dynadoc "1.6.5-SNAPSHOT"
+(defproject dynadoc "1.6.5"
   :description "A dynamic documentation generator"
   :url "https://github.com/oakes/Dynadoc"
   :license {:name "Public Domain"
