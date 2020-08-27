c0c9a4eb68a9e3d9067e40f3b2afff6ee79e3e93
oakes <zach@sekao.net>
2020-08-27 11:59:47 -0400

    Prevent figwheel from killing file watcher

--- src/dynadoc/watch.clj
+++ src/dynadoc/watch.clj
@@ -9,16 +9,26 @@
 (defonce *channel->uri (atom {}))
 (defonce *cljs-info (atom nil))
 
+(def watcher
+  {:paths [(.getCanonicalPath (io/file "."))]
+   :handler (fn [ctx {:keys [kind file]}]
+              (when (#{:create :modify} kind)
+                (when (or (str/ends-with? (.getName file) ".cljs")
+                          (str/ends-with? (.getName file) ".cljc"))
+                  (try
+                    (swap! *cljs-info #(static/read-cljs-file % file))
+                    (catch Exception _))))
+              ctx)})
+
 (defn init-watcher! []
   (reset! *cljs-info (static/get-cljs-nses-and-vars))
-  (hawk/watch! [{:paths [(.getCanonicalPath (io/file "."))]
-                 :handler (fn [ctx {:keys [kind file]}]
-                            (when (#{:create :modify} kind)
-                              (when (str/ends-with? (.getName file) ".cljs")
-                                (try
-                                  (swap! *cljs-info #(static/read-cljs-file % file))
-                                  (catch Exception _))))
-                            ctx)}]))
+  (try
+    ;; if figwheel is on the classpath, use its file watching stuff
+    ;; because otherwise it will kill the file watcher we start here
+    (require 'figwheel.main.watching)
+    ((resolve 'figwheel.main.watching/add-watch!) ::watcher watcher)
+    (catch Exception _
+      (hawk/watch! [watcher]))))
 
 (defn watch-request [request]
   (with-channel request channel
