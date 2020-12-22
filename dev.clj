9ee9b8ce7c064f1e35895f628f62e76e55bd21c8
oakes <zach@sekao.net>
2020-12-22 16:58:36 -0500

    Remove orchestra/expound

--- dev.clj
+++ dev.clj
@@ -1,12 +1,10 @@
 (require
-  '[orchestra.spec.test :as st]
-  '[expound.alpha :as expound]
+  '[clojure.spec.test.alpha :as st]
   '[clojure.spec.alpha :as s]
   '[figwheel.main :as figwheel]
   '[dynadoc.core :as dynadoc]
   '[dynadoc.examples])
 
 (st/instrument)
-(alter-var-root #'s/*explain-out* (constantly expound/printer))
 (dynadoc/dev-start {:port 5000 :dev? true})
 (figwheel/-main "--build" "dev")
--- src/dynadoc/dev.cljs
+++ src/dynadoc/dev.cljs
@@ -1,9 +1,7 @@
 (ns dynadoc.dev
   (:require [dynadoc.core :as c]
             [dynadoc.examples]
-            [orchestra-cljs.spec.test :as st]
-            [expound.alpha :as expound]
+            [clojure.spec.test.alpha :as st]
             [clojure.spec.alpha :as s]))
 
 (st/instrument)
-(set! s/*explain-out* expound/printer)
