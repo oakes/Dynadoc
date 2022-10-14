9d819b08311f1ed9b29b92940a43ac2baf36c8c3
oakes <zach@sekao.net>
2022-10-14 11:36:57 +0200

    Update lein plugin again

--- lein-dynadoc/project.clj
+++ lein-dynadoc/project.clj
@@ -1,11 +1,11 @@
-(defproject dynadoc/lein-dynadoc "1.7.5"
+(defproject dynadoc/lein-dynadoc "1.7.5.1"
   :description "A conveninent Dynadoc launcher for Leiningen projects"
   :url "https://github.com/oakes/Dynadoc"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[dynadoc "1.7.4"]
-                 [leinjacker "0.4.2"]
-                 [org.clojure/tools.cli "0.3.5"]]
+  :dependencies [[dynadoc "1.7.5"]
+                 [leinjacker "0.4.3"]
+                 [org.clojure/tools.cli "1.0.214"]]
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]]
   :eval-in-leiningen true)
