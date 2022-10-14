eac412e3ffb3a03798220d2d4e1dc7d0f822effd
oakes <zach@sekao.net>
2022-10-14 11:39:40 +0200

    Start 1.7.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dynadoc "1.7.5"
+(defproject dynadoc "1.7.6-SNAPSHOT"
   :description "A dynamic documentation generator"
   :url "https://github.com/oakes/Dynadoc"
   :license {:name "Public Domain"
